4ba99fa4c2296a6afbb2021b6d687c759871b3cf
Kevin Escudero <110898636+LuisKevinEscudero@users.noreply.github.com>
2023-11-30 13:36:36 +0100

    Add files via upload

---
+++ junit_app/src/main/java/org/example/junitapp/exceptions/DineroInsuficienteException.java
@@ -0,0 +1,9 @@
+package org.example.junitapp.exceptions;
+
+public class DineroInsuficienteException extends RuntimeException{
+	
+	public DineroInsuficienteException(String message) {
+		super(message);
+	}
+
+}
---
+++ junit_app/src/main/java/org/example/junitapp/models/Cuenta.java
@@ -0,0 +1,69 @@
+package org.example.junitapp.models;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import org.example.junitapp.exceptions.DineroInsuficienteException;
+
+public class Cuenta {
+
+	private String persona;
+	private BigDecimal saldo;
+	private Banco banco;
+	
+	public Cuenta(String persona, BigDecimal saldo) {
+		this.saldo = saldo;
+		this.persona = persona;
+	}
+	
+	public String getPersona() {
+		return persona;
+	}
+	
+	public void setPersona(String persona) {
+		this.persona = persona;
+	}
+	
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+
+	public void debito(BigDecimal monto) {
+		BigDecimal nuevoSaldo = saldo.subtract(monto);
+		
+		if(nuevoSaldo.compareTo(BigDecimal.ZERO) < 0) {
+			throw new DineroInsuficienteException("dinero insuficiente");
+		}
+		this.saldo = nuevoSaldo;
+	}
+	
+	public void credito(BigDecimal monto) {
+		this.saldo = this.saldo.add(monto);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cuenta other = (Cuenta) obj;
+		return Objects.equals(persona, other.persona) && Objects.equals(saldo, other.saldo);
+	}
+	
+	
+}
